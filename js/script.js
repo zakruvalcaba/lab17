c6aa2109cde1df5fd2fae1ceb3803f1083a5f7c5
zakruvalcaba <zakruvalcaba@gmail.com>
2021-04-22 09:53:45 -0700

    updated lab and updated to ES6

--- js/script.js
+++ js/script.js
@@ -1,62 +1,66 @@
-/*eslint-env browser*/
+const $ = (id) => {
+    return document.getElementById(id);
+};
 
-//CREATE A JUKEBOX CLASS
-var Jukebox = function () {
-    "use strict";
-    var albums = [], self;
-    self = {
-        //ADD EACH ALBUM TO THE ALBUMS ARRAY
-        addAlbum: function (album) {
-            albums.push(album);
-        },
-        //RETURN THE ALBUM THAT'S BEEN PLAYED THE MOST
-        favoriteAlbum: function () {
-            var max = 0, fav, i;
-            
-            for (i = 0; i < albums.length; i += 1) {
-                if (albums[i].played > max) {
-                    max = albums[i].played;
-                    fav = albums[i];
-                }
-            }
-            return fav.display();
-        }
-    };
-    return self;
-};
-//CREATE AN ALBUM CLASS
-var Album = function (artist, album) {
-    "use strict";
-    var self = {
-        played: 0,
-        //INCREASE THE AMOUNT OF TIMES AN ALBUM HAS BEEN PLAYED
-        play: function () {
-            this.played += 1;
-        },
-        //DISPLAY THE AMOUNT OF TIMES ALBUM HAS BEEN PLAYED
-        display: function () {
-            return artist + " \"" + album + "\". It's been played " + this.played + " times.";
-        }
-    };
-    return self;
-};
-//CREATE JUKEBOX
-var jukebox = new Jukebox();
-//CREATE 3 ALBUMS
-var album1 = new Album("Shakira", "Knock on my Door");
-var album2 = new Album("A Tribe Called Quest", "The Low End Theory");
-var album3 = new Album("Imagine Dragons", "Hell and Silence");
-//ADD ALL 3 ALBUMS TO JUKEBOX
-jukebox.addAlbum(album1);
-jukebox.addAlbum(album2);
-jukebox.addAlbum(album3);
-//PLAY ALBUMS
-album1.play();
-album1.play();
-album1.play();
-album2.play();
-album3.play();
-album1.play();
-album3.play();
-//DISPLAY FAVORITE ALBUM BASED ON PLAYS
-window.console.log("Your favorite album is: " + jukebox.favoriteAlbum());
+function calculateDays() {
+    let event;
+    let dt;
+    let year;
+    let date;
+    let today;
+    let oneDay;
+    let days;
+
+    // SELECT VALUES FROM EVENT AND DATE TEXT BOXES
+    event = $('event').value;
+    dt = $('date').value;
+
+    // MAKE SURE THAT EVENT AND DATE TEXT BOXES ARE NOT EMPTY
+    if (event.length === 0 || dt.length === 0) {
+        $('message').innerHTML = 'Please enter an event and a date.';
+        return;
+    }
+
+    // MAKE SURE THE DATE STRING HAS SLASHES
+    if (dt.indexOf('/') === -1) {
+        $('message').innerHTML = 'SLASH: Please check the date format (Ex: XX/XX/XXXX).';
+        return;
+    }
+
+    // GET YEAR FROM EVENT DATE STRING AND VERIFY THAY IT IS 4 DIGIT/NUMERIC
+    year = dt.substring(dt.length - 4);
+    if (isNaN(year)) {
+        $('message').innerHTML = 'YEAR: Please check the date format (Ex: XX/XX/XXXX).';
+        return;
+    }
+
+    // CONVERT THE EVENT DATE STRING TO DATE OBJECT AND MAKE SURE IT IS VALID
+    date = new Date(dt);
+    if (date === 'Invalid Date') {
+        $('message').innerHTML = 'DATE FORMAT: Please check the date format (Ex: XX/XX/XXXX).';
+        return;
+    }
+
+    // CALCULATE DAYS
+    today = new Date();
+    // HOURS * MINUTES * SECONDS * MILLISECONDS = ONE DAY
+    oneDay = 24 * 60 * 60 * 1000 
+    // USER'S DATE - TODAY'S DATE / ONE DAY = NUMBER OF DAYS
+    days = (date.getTime() - today.getTime()) / oneDay;
+    // ROUND NUMBER OF DAYS UP
+    days = Math.ceil(days);
+
+    // CREATE AND DISPLAY MESSAGE
+    if (days === 0) {
+        $('message').innerHTML = `Today is ${event.toUpperCase()}\n${date.toDateString()}`;
+    } else if (days < 0) {
+        $('message').innerHTML = `${event.toUpperCase()} happened ${Math.abs(days)} days ago.\n${date.toDateString()}`;
+    } else if (days > 0) {
+        $('message').innerHTML = `${Math.abs(days)} days until ${event.toUpperCase()}.\n${date.toDateString()}`;
+    }
+}
+
+window.addEventListener('load', () => {
+    $('countdown').addEventListener('click', calculateDays);
+    $('event').focus();
+});
