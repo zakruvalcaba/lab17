b09e413370d227ba9d49915216ec7700da04e230
Zak Ruvalcaba <zakruvalcaba@gmail.com>
2024-01-04 09:37:17 -0800

    Updated script

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const $ = (id) => document.getElementById(id)
 function getRandomNumber() {
     // CREATE RANDOM NUMBER
     let random = Math.random()
-    // RANDOM NUMBER BETWEEN 0 AND 6
+    // RANDOM NUMBER BETWEEN 1 AND 6
     random = Math.floor(random * 6)
     // ADD 1 SO RANDOM IS NEVER 0 (1-6)
     random = random + 1
@@ -19,6 +19,7 @@ function changePlayer() {
     } else {
         $('current').innerHTML = $('player1').value
     }
+
     // SET DIE AND TOTAL TEXT BOXES TO 0
     $('die').value = '0'
     $('total').value = '0'
@@ -29,23 +30,24 @@ function newGame() {
     // SET SCORES TO 0
     $('score1').value = '0'
     $('score2').value = '0'
-    // CHECK TO SEE IF PLAYER NAMES EXIST
-    if ($('player1').value !== '' || $('player2').value !== '') {
+
+    // CHECK TO SEE IF PLAYER NAMES EXISTS
+    if ($('player1').value !== '' && $('player2').value !== '') {
         $('turn').setAttribute('class', 'open')
         changePlayer()
     } else {
-        $('turn').removeAttribute('class')
+        $('turn').setAttribute('class', '')
         alert('Please enter two player names to begin.')
     }
 }
 
-function rollDice() {
-    // GET THE TOTAL
+function rollDie() {
+    // GET THE TOTAL SCORE
     let total = parseInt($('total').value)
-    // GET RANDOM NUMBER BETWEEN 1-6
+    // ROLL DIE
     let die = getRandomNumber()
     // IF DIE IS 1, 0 OUT TOTAL AND SWITCH PLAYER
-    // OTHERWISE INCREMENT USER'S TEMPORARY TOTAL
+    // OTHERWISE INCREMENT USER'S TEMP TOTAL
     if (die <= 1) {
         total = 0
         changePlayer()
@@ -66,19 +68,18 @@ function holdTurn() {
     } else {
         score = $('score2')
     }
-    // ADD CURRENT SCORE TO TOTAL SCORE
+    // ADD CURRENT SCORE TO THE TOTAL SCORE
     score.value = parseInt(score.value) + total
-    // IF TOTAL SCORE IS 100, PLAYER WINS, START NEW GAME
-    // OTHERWISE CHANGE PLAYER
+    // IF TOTAL SCORE IS 100, PLAYER WINS, START NEW GAME OTHERWISE CHANGE PLAYER
     if (score.value >= 100) {
         alert(`${$('current').innerHTML} wins!`)
         newGame()
     } else {
         changePlayer()
-    }
+    } 
 }
 
 // EVENT LISTENERS FOR 'NEW GAME', 'ROLL', AND 'HOLD' BUTTONS
 $('new_game').addEventListener('click', newGame)
-$('roll').addEventListener('click', rollDice)
+$('roll').addEventListener('click', rollDie)
 $('hold').addEventListener('click', holdTurn)
